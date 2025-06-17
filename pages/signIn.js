f86a5c36fcb1d7d8461d4624fff0ff6f03893843
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2025-06-17 13:42:36 +0500

    Authentication pages completed

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,22 +1,23 @@
-// import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-// import { auth } from "../firebaseconfig";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { auth } from "./firebaseconfig.js";
 
-// const password = document.querySelector("#passwordForm")
-// const submit = document.querySelector("#submitBtn")
-// const email = document.querySelector("#emailForm")
+const password = document.querySelector("#passwordForm")
+const submit = document.getElementById("submit")
+const email = document.querySelector("#emailForm")
 
 
-// submit.addEventListener('click' , event => {
-//     event.preventDefault()
+submit.addEventListener('click', event => {
+    event.preventDefault()
 
-// createUserWithEmailAndPassword(auth, email.value, password.value)
-// .then((userCredential) => {
-//     const user = userCredential.user;
-//     console.log(user);
-//   })
-//   .catch((error) => {
-//     console.log("Error:", error.message);
-//     window.location = "signIn.html"
-//   });
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user)
+            window.location = "chat.html"
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage)
+        });
 
-// })
+})
--- pages/signUp.js
+++ pages/signUp.js
@@ -1,41 +1,9 @@
-// import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-// import { auth } from "../firebaseconfig";
-
-// const password = document.querySelector("#passwordForm")
-// const submit = document.querySelector("#submitBtn")
-// const email = document.querySelector("#emailForm")
-// const fullName = document.querySelector("#fullName")
-
-// submit.addEventListener('click' , event =>{
-//     event.preventDefault()
-//     console.log(fullName);
-
-//     createUserWithEmailAndPassword(auth, email.value, password.value)
-//     .then((userCredential) => {
-//       const user = userCredential.user;
-//       console.log(user);
-    
-//   })
-//     .catch((error) => {
-//     const errorMessage = error.message;
-//     console.log(errorMessage);
-
-//     window.location = "logIn.html"
-    
-//   });
-
-//     // console.log(fullName.value);
-//     console.log(email.value);
-//     console.log(password.value);
-// })  
-
-
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 import { auth } from "./firebaseconfig.js";
 
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const submit = document.querySelector("#submit");
+const submit = document.getElementById("submit");
 
 submit.addEventListener("click", (event) => {
   event.preventDefault();
@@ -45,11 +13,10 @@ submit.addEventListener("click", (event) => {
       const user = userCredential.user;
       console.log("User signed up:", user);
       alert("Signup successful!");
-      // Redirect to a dashboard or login page
-      // window.location = "login.html";
+      window.location = "signIn.html"
     })
     .catch((error) => {
       console.log("Signup error:", error.message);
-      alert(error.message); // Show the Firebase error
-    });
-});
+      alert(error.message); 
+    })
+})
