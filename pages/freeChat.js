2c68a5c470c22b6f57ebd5805b623df4eb2dab77
jsrayanyaqoob <rayanyaqoob83@gmail.com>
2025-06-26 14:40:42 +0500

    api added chatbot and user msg UI in process

--- pages/freeChat.js
+++ pages/freeChat.js
@@ -1,21 +1,20 @@
-import GoogleGenAI from 'https://cdn.jsdelivr.net/npm/@google/genai';
+const inputVal = document.getElementById("inputVal");
+const heading = document.getElementById("heading");
 
+const botMessages = []
 
-const api_key = 'AIzaSyBlHiEte7wAH2AFLN6Q3ihCF-VQu4uYFG0'
-const inputVal = document.getElementById("inputVal")
-const heading = document.getElementById("nheading")
+inputVal.addEventListener("keydown", async (e) => {
+  if (e.key === "Enter") {
+    const userInput = inputVal.value;
+    
+    const res = await fetch("http://localhost:3000/generate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ prompt: userInput }),
+    });
 
-
-const ai = new GoogleGenAI({ apiKey: api_key });
-
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: inputVal,
-  });
-  console.log(response.text);
-}
-
-main();
-
-// console.log(main())8
+    const data = await res.json()
+    const result = data?.candidates?.[0]?.content?.parts?.[0]?.text
+    heading.textContent = result
+  }
+});
